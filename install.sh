03c93d46e205728ca47bcfc6659a1764354c197b
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-06-27 15:05:24 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+sudo apt install python3-pyaudio -y
+
+sudo apt install flac -y
+ 
+sudo pip3 install speechrecognition
+
+sudo usermod -a -G spi,gpio pi
+
+sudo apt install build-essential python3-dev python3-pip libfreetype6-dev libjpeg-dev libopenjp2-7 libtiff5 -y
+
+sudo pip3 install setuptools
+
+sudo pip3 install --upgrade luma.led_matrix
+
+
+sudo apt install python3-pyaudio -y
+
+sudo apt install flac -y
+
+sudo pip3 install speechrecognition
+
