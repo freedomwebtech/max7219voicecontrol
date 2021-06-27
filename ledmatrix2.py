128e8f6c0d78db0a3836699fdc8ed5d75d2500eb
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-06-27 15:05:43 +0530

    Add files via upload

---
+++ ledmatrix2.py
@@ -0,0 +1,26 @@
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+from datetime import datetime
+import time
+from speech import audio
+import sys
+import os
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=4, block_orientation=-90, blocks_arranged_in_reverse_order=True)
+device.contrast(16)
+
+a= audio()
+
+
+def test():
+    with canvas(device) as draw:
+          show_message(device, a, fill="red",font=(CP437_FONT),scroll_delay=0.08)
+     
+          
+     
+while True:
+      test()
