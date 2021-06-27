e31d6dd66528343fec2939d2233d308c2c463557
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-06-27 15:08:47 +0530

    Update ledmatrix2.py

--- ledmatrix2.py
+++ ledmatrix2.py
@@ -4,16 +4,13 @@ from luma.core.render import canvas
 from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
-from datetime import datetime
 import time
 from speech import audio
-import sys
-import os
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=-90, blocks_arranged_in_reverse_order=True)
 device.contrast(16)
 
-a= audio()
+a=audio()
 
 
 def test():
